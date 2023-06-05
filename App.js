c95e9f430ebf1187140eb6a797eb97a75a4b8ad1
Omkar Ghankute <123437786+Unfazed7@users.noreply.github.com>
2023-06-05 20:36:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,59 @@
+import React from "react";
+
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  useLocation,
+} from "react-router-dom";
+
+import img1 from "./images/yaari.png";
+import "./Navbar.css";
+import About from "./About";
+import Contact from "./Contact";
+import Home from "./Home";
+//import './App.css';
+
+const Navbar = () => {
+  const location = useLocation();
+
+  return (
+    <nav className="navbar">
+      <div className="navbar-left">
+        <ul className="nav-links">
+          <li className={location.pathname === "/" ? "active" : ""}>
+            <Link to="./Home">Home</Link>
+          </li>
+          <li className={location.pathname === "/about" ? "active" : ""}>
+            <Link to="./About">About</Link>
+          </li>
+          <li className={location.pathname === "/contact" ? "active" : ""}>
+            <Link to="./Contact">Contact</Link>
+          </li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+        <img src={img1} alt="" className="logo" />
+        <input type="text" placeholder="Search" className="search-bar" />
+      </div>
+    </nav>
+  );
+};
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/Home" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Contact" element={<Contact />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
