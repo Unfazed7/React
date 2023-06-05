86c5e3b4389ce3f064501bb9b2aa9d407a1ad53f
Omkar Ghankute <123437786+Unfazed7@users.noreply.github.com>
2023-06-05 20:18:12 +0530

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -3,8 +3,7 @@ import React from 'react';
 const Home = () => {
   return (
     <div>
-      <h1>Welcome to the Home Page</h1>
-      {/* Add content for your home page */}
+      Welcome to the Home Page
     </div>
   );
 };
